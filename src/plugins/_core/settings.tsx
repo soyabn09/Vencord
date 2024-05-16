d9e571825e8a6840d7fc1b992bdd7e82c2e0175f
Soyab Nandhla <admin@soyabnandhla.me>
2024-05-16 18:10:16 +0100

    Update settings.tsx

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -224,7 +224,7 @@ export default definePlugin({
     getInfoRows() {
         const { electronVersion, chromiumVersion, additionalInfo } = this;
 
-        const rows = [`Vencord ${gitHash}${additionalInfo}`];
+        const rows = [`Vencord ${VERSION} (${gitHash})${additionalInfo}`];
 
         if (electronVersion) rows.push(`Electron ${electronVersion}`);
         if (chromiumVersion) rows.push(`Chromium ${chromiumVersion}`);
